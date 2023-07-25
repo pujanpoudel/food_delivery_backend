705a78918cfd0a1b70bf7903e962c2749274ea53
pujan poudel <pujanpoudelj7@gmail.com>
2023-07-25 23:08:24 +0545

    update

--- shopping-app/app/Http/Controllers/Api/V1/ConfigController.php
+++ shopping-app/app/Http/Controllers/Api/V1/ConfigController.php
@@ -55,4 +55,24 @@ class ConfigController extends Controller
         //return response()->json(['message'=>trans('messages.we_are_temporarily_unavailable_in_this_area')], 403);
          return response()->json(['zone_id'=>1], 200);
     }
+    public function place_api_autocomplete(request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'search_text = >'required,
+        ]);
+
+        if($validator->errors()->count()>0){
+            return response()->json(
+                ['errors' => Helpers::error_processor($validator)],
+                403);
+        }
+        $respons4 = Http::get(
+            https://maps.googleapis.com/maps/api/place/autocomplete/json?input='
+            •$request ['search_text ']
+            .'Skey='
+            .'AIzaSyCMESvjp3G5FtPnukZ28_GVOuFSvEhSS9c'
+        );
+        return $response->json();
+        
+    }
 }
