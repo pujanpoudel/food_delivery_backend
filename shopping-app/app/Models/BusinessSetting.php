30ab7b42dc72492dcc71f95a5eae93a8a7af5550
pujan poudel <pujanpoudelj7@gmail.com>
2023-08-14 07:32:01 +0545

    updated paypal static file

---
+++ shopping-app/app/Models/BusinessSetting.php
@@ -0,0 +1,11 @@
+<?php
+
+ namespace App\Models;
+
+ use Illuminate\DataBase\Eloquent\Factories;
+ use Illuminate\DataBase\Eloquent\Model;
+
+ class BusinessSettting extends Models
+ {
+    use HasFactory;
+ }
